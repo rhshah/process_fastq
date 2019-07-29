7a2efec30813d855d1b9971f6fe14821bf7826d1
Ronak Shah <rons.shah@gmail.com>
2019-07-29 15:46:17 -0400

    Update process_fastq.py

--- process_fastq/process_fastq.py
+++ process_fastq/process_fastq.py
@@ -44,11 +44,12 @@ def run(sample_id, request_id, run_id, fastq_path, output_path, cutadapt_path):
     logger.info("procees_fastq: fastq_path: %s", fastq_path)
     logger.info("procees_fastq: output_path: %s", output_path)
     logger.info("procees_fastq: cutadapt_path: %s", cutadapt_path)
-    run_dict = {}
+    run_dict = OrderedDict(dict)
     for id in run_id:
         glob_file_path = hp.make_path(fastq_path, id, request_id, sample_id)
         logger.info("process_fastq: the path to search for files: %s",
                     glob_file_path)
+        run_dict.update
         run_dict[id]['path'] = glob_file_path
         fastq_list = hp.get_fastq(glob_file_path)
         run_dict[id]['fastq_list'] = fastq_list
