528b7414fa7654cb947a9cac5a5501167242049a
Ronak Shah <rons.shah@gmail.com>
2019-07-29 14:50:03 -0400

    Givving unclashing names

--- process_fastq/helper.py
+++ process_fastq/helper.py
@@ -35,23 +35,23 @@ def read_excel(file):
     return pdataframe
 
 
-def make_path(path, run_id, request_id, sample_id):
+def make_path(dir_path, run_id, request_id, sample_id):
     logger.info("helper: Making file path to search for files")
     glob_run_id = "*" + run_id + "*"
     glob_request_id = "*" + request_id + "*"
     glob_sample_id = "*" + sample_id + "*"
-    glob_path = os.path.join(path, glob_run_id, glob_request_id, glob_sample_id)
+    glob_path = os.path.join(dir_path, glob_run_id, glob_request_id, glob_sample_id)
     glob_path = glob.glob(glob_path)
     logger.info("helper: Finished making file path to search for files")
     return glob_path
 
 
-def get_fastq(path):
+def get_fastq(dir_path):
     logger.info("helper: Globbing fastq.gz file")
     R1_pattern = "*R1*.gz"
     R2_pattern = "*R2*.gz"
-    glob_path_R1 = os.path.join(path, R1_pattern)
-    glob_path_R2 = os.path.join(path, R2_pattern)
+    glob_path_R1 = os.path.join(dir_path, R1_pattern)
+    glob_path_R2 = os.path.join(dir_path, R2_pattern)
     glob_path_R1 = glob.glob(glob_path_R1)
     glob_path_R2 = glob.glob(glob_path_R2)
     logger.info("helper: Done globbing fastq.gz file")
