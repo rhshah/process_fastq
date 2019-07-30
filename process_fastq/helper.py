bdf8b2c93b2f22c17cf1c4758b428afc4b02fd80
Ronak Shah <rons.shah@gmail.com>
2019-07-30 11:29:59 -0400

    Removing safe get

--- process_fastq/helper.py
+++ process_fastq/helper.py
@@ -101,12 +101,3 @@ def deep_get(dictionary, keys, default=None):
         keys.split("."),
         dictionary,
     )
-
-
-def safeget(dct, *keys):
-    for key in keys:
-        try:
-            dct = dct[key]
-        except KeyError:
-            return None
-    return dct
