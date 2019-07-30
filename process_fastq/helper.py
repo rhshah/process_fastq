9543e6b3d3189d28475e47d0fa072020cb6a9b04
Ronak Shah <rons.shah@gmail.com>
2019-07-30 11:27:34 -0400

    Adding safeget test

--- process_fastq/helper.py
+++ process_fastq/helper.py
@@ -101,3 +101,12 @@ def deep_get(dictionary, keys, default=None):
         keys.split("."),
         dictionary,
     )
+
+
+def safeget(dct, *keys):
+    for key in keys:
+        try:
+            dct = dct[key]
+        except KeyError:
+            return None
+    return dct
