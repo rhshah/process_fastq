8d12d42fb99a1d83f8b7f0bfdce0fe70833df359
Ronak Shah <rons.shah@gmail.com>
2019-08-01 13:50:19 -0400

    Update helper.py

--- process_fastq/helper.py
+++ process_fastq/helper.py
@@ -42,29 +42,36 @@ def make_path(dir_path, run_id, request_id, sample_id):
     if run_id:
         glob_run_id = "*" + run_id + "*"
     else:
-        glob_run_id = "*"
+        glob_run_id = "**"
     logger.debug("helper: make_path: glob_run_id: %s", glob_run_id)
     if request_id:
         glob_request_id = "*" + request_id + "*"
     else:
-        glob_request_id = "*"
+        glob_request_id = "**"
     logger.debug("helper: make_path: glob_request_id: %s", glob_request_id)
     glob_sample_id = "*" + sample_id + "*"
     logger.debug("helper: make_path: glob_sample_id: %s", glob_sample_id)
     glob_path = os.path.join(dir_path, glob_run_id, glob_request_id, glob_sample_id)
     logger.debug("helper: make_path: glob_path: %s", glob_path)
-    glob_path = glob.glob(glob_path)
-    if len(glob_path) > 1:
-        for path in glob_path:
-            if ('A1' in path):
-                glob_path = [path,]
-                break
-            else:
-                logging.error("helper: make_path: Found multiple path in to fastq folder for sample %s, %s: ", sample_id, glob_path)
-                exit(1)
+    glob_path = glob.glob(glob_path, recursive=True)
+    if run_id is None and request_id is None:
+        pass
+    else:
+        if len(glob_path) > 1:
+            for path in glob_path:
+                if "A1" in path:
+                    glob_path = [path]
+                    break
+                else:
+                    logging.error(
+                        "helper: make_path: Found multiple path in to fastq folder for sample %s, %s: ",
+                        sample_id,
+                        glob_path,
+                    )
+                    exit(1)
     logger.debug("helper: make_path: glob glob_path: %s", glob_path)
     logger.info("helper: make_path: Finished making file path to search for files")
-    if(run_id is None and request_id is None):
+    if run_id is None and request_id is None:
         return glob_path
     else:
         return "".join(glob_path)
@@ -84,7 +91,9 @@ def get_fastq(dir_path):
 
 
 def get_fastq_read_length(fastq_list):
-    logger.info("helper: get_fastq_read_length: getting the read length of each fastq file")
+    logger.info(
+        "helper: get_fastq_read_length: getting the read length of each fastq file"
+    )
     read_length_list = []
     for fastq in fastq_list:
         cmd = (
@@ -104,14 +113,18 @@ def get_fastq_read_length(fastq_list):
             shell=True,
         )
         logger.debug(
-            "helper: get_fastq_read_length: the commandline is %s", cmd.encode("unicode_escape").decode("utf-8")
+            "helper: get_fastq_read_length: the commandline is %s",
+            cmd.encode("unicode_escape").decode("utf-8"),
         )
         stdout, stderr = out.communicate()
         if stderr is None:
             logger.debug("helper: get_fastq_read_length: Read: %s", stdout)
             read_length = len(stdout)
         else:
-            logger.error("helper: get_fastq_read_length: could not calcualte the read for: %s", fastq)
+            logger.error(
+                "helper: get_fastq_read_length: could not calcualte the read for: %s",
+                fastq,
+            )
             exit(1)
         read_length_list.append(read_length)
     return read_length_list
