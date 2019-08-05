12d303b25b47da41955686d976e3300771825c50
Ronak Shah <rons.shah@gmail.com>
2019-08-05 17:05:53 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,45 +5,41 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', ]
+requirements = ["pandas==0.25.0", "click==7.0", "click-log==0.3.2", "cutadapt==2.4"]
 
-setup_requirements = [ ]
+setup_requirements = ["pandas==0.25.0", "click==7.0", "click-log==0.3.2", "cutadapt==2.4"]
 
-test_requirements = [ ]
+test_requirements = ["pandas==0.25.0", "click==7.0", "click-log==0.3.2", "cutadapt==2.4"]
 
 setup(
     author="Ronak Hasmukh Shah",
-    author_email='rons.shah@gmail.com',
+    author_email="rons.shah@gmail.com",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.7",
     ],
     description="This package will help process, merge and link fastq in user specified directory from manifest file",
-    entry_points={
-        'console_scripts': [
-            'process_fastq=process_fastq.cli:main',
-        ],
-    },
+    entry_points={"console_scripts": ["process_fastq=process_fastq.cli:main"]},
     install_requires=requirements,
     license="Apache Software License 2.0",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='process_fastq',
-    name='process_fastq',
-    packages=find_packages(include=['process_fastq']),
+    keywords="process_fastq",
+    name="process_fastq",
+    packages=find_packages(include=["process_fastq"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/rhshah/process_fastq',
-    version='0.1.0',
+    url="https://github.com/rhshah/process_fastq",
+    version="0.1.0",
     zip_safe=False,
 )
