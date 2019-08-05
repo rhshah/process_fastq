1d6bf0f326ce33ba8c42d5fe18fd79718dfbe47e
Ronak Shah <rons.shah@gmail.com>
2019-08-05 00:52:26 -0400

    Update run_cutadapt.py

--- process_fastq/run_cutadapt.py
+++ process_fastq/run_cutadapt.py
@@ -29,7 +29,7 @@ def run(cutadapt_path, output_path, fastq_list, trim_length):
     sample_id_2 = p_path_2.name
     tmp_fo = tempfile.TemporaryDirectory(dir=output_path, prefix="cutadapt_")
     out_file_path_1 = os.path.join(tmp_fo.name, sample_id_1)
-    out_file_path_1 = os.path.join(tmp_fo.name, sample_id_2)
+    out_file_path_2 = os.path.join(tmp_fo.name, sample_id_2)
     cmd = (
         cutadapt_path
         + " --action none -l "
