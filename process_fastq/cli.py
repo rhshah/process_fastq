2e0c20974e68111f141dddec2b843b89962d7d93
Ronak Shah <rons.shah@gmail.com>
2019-08-06 14:24:49 -0400

    Develop (#7)
    * Update .travis.yml
    
    * trying travis
    
    * :sob:
    
    * :recycle:
    
    * Using only python3.7
    
    * TRAVIS :sob:
    
    * Update cli.py
    
    * Update setup.py
    
    * :facepunch:
    
    * :sob:
    
    * Update Makefile
    
    * Update Makefile
    
    * Update test_process_fastq.py
    
    * Update tox.ini
    
    * Update tox.ini
    
    * Bump version: 0.1.0 → 0.1.1
    
    * making stantdard version
    
    * Update setup.cfg
    
    * Update .travis.yml

--- process_fastq/cli.py
+++ process_fastq/cli.py
@@ -39,7 +39,7 @@ Description: console script for running process_fastq
 """
 
 __all__ = []
-__version_info__ = ("0", "1", "0")
+__version_info__ = ("1", "0", "0")
 __version__ = ".".join(__version_info__)
 __date__ = "2019-07-27"
 __updated__ = "2019-07-28"
