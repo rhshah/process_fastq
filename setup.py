f96f1d117007a5c5f50d813b8910cad0e80078e8
Ronak Shah <rons.shah@gmail.com>
2019-08-06 14:50:18 -0400

    Bump version: 2.0.0 → 2.0.1-dev0

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/rhshah/process_fastq",
-    version="version='2.0.0'",
+    version="version='2.0.1-dev0'",
     zip_safe=False,
 )
