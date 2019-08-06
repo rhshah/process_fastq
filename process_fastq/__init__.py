78c63f2d4cdebf3ef89fc189001e2d7c5683a5bc
Ronak Shah <rons.shah@gmail.com>
2019-08-06 15:02:39 -0400

    Bump version: 2.0.2 → 2.0.3

--- process_fastq/__init__.py
+++ process_fastq/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Ronak Hasmukh Shah"""
 __email__ = 'rons.shah@gmail.com'
-__version__ = '2.0.2'
+__version__ = '2.0.3'
--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/rhshah/process_fastq",
-    version="version='2.0.2'",
+    version="version='2.0.3'",
     zip_safe=False,
 )
